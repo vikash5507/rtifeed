e4d5781a90dbcd1742cd2282f3416c1c4b856add
Paarth <paarth.n@gmail.com>
2015-10-20 02:39:22 +0530

    Kick start models

---
+++ RTIFeed/settings.py
@@ -0,0 +1,88 @@
+"""
+Django settings for RTIFeed project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8b0e$m=8_#-nj7-b=9c8s9h9)knu8-bg&5n&qni)9_pe+ye(xz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rtiapp'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'RTIFeed.urls'
+
+WSGI_APPLICATION = 'RTIFeed.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'RTIFeed',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ rtiapp/models.py
@@ -0,0 +1,211 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#TO DO
+# max_length attributes
+
+# https://docs.djangoproject.com/en/1.8/ref/contrib/auth/
+class State(models.Model):
+	def __unicode__(self):
+		return self.user.state_name
+
+	state_name = models.CharField(max_length = 200)
+	capital_name = models.CharField(max_length = 200)
+	latitude = models.FloatField(null = True)
+	longitude = models.FloatField(null = True)
+
+class Address(models.Model):
+	def __unicode__(self):
+		return self.user.address_line
+
+	address_line = models.CharField(max_length = 200, null = True)
+	state = models.ForeignKey(State, on_delete = models.SET_NULL, null = True)
+	city = models.CharField(max_length = 200, null = True)
+	pincode = models.CharField(max_length = 200, null = True)
+	latitude = models.FloatField(null = True)
+	longitude = models.FloatField(null = True)
+	entry_date = models.DateTimeField()
+	update_date = models.DateTimeField(auto_now_add=True)
+
+class User_profile(models.Model):
+	def __unicode__(self):
+		return self.user.username
+
+	user = models.OneToOneField(User, primary_key = True)
+	profilePicture = models.ImageField(upload_to  = 'static/profile_picture', default = 'static/profile_picture/default.jpg')
+	reputation = models.FloatField(default = 10)
+	gender = models.CharField(max_length = 200, null = True)
+	date_of_birth = models.DateTimeField(null = True)
+	address = models.ForeignKey(Address, null = True)
+	bio_description = models.CharField(max_length = 200)
+	entry_date = models.DateTimeField()
+	update_date = models.DateTimeField(auto_now_add=True)
+
+class RTI_query(models.Model):
+	def __unicode__(self):
+		return self.description
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	query_text = models.TextField()
+	description = models.CharField(max_length = 200)
+	rti_number = models.CharField(max_length = 50)
+	rti_file_date = models.DateTimeField()
+	response_status = models.BooleanField(default = False)
+	query_type = models.CharField(max_length = 50)
+	entry_date = models.DateTimeField()
+	update_date = models.DateTimeField(auto_now_add=True)
+
+# STATE RTI QUERIES
+class State_department(models.Model):
+	def __unicode__(self):
+		return self.department_name
+
+	state = models.ForeignKey(State, on_delete = models.CASCADE)
+	department_name = models.CharField(max_length = 200)
+
+class State_authority(models.Model):
+	def __unicode__(self):
+		return self.authority_name
+
+	department = models.ForeignKey(State_department, on_delete = models.CASCADE)
+	authority_name = models.CharField(max_length = 200)
+
+class State_RTI_query(models.Model):
+	def __unicode__(self):
+		return self.rti_query
+
+	rti_query = models.ForeignKey(RTI_query, primary_key = True)
+	department = models.ForeignKey(State_department, on_delete = models.SET_DEFAULT, default = 1)
+	authority = models.ForeignKey(State_authority, on_delete = models.SET_NULL, null = True)
+
+#CENTRAL RTI QUERIES
+class Central_department(models.Model):
+	def __unicode__(self):
+		return self.department_name
+
+	department_name = models.CharField(max_length = 200)
+
+class Central_authority(models.Model):
+	def __unicode__(self):
+		return self.authority_name
+
+	department = models.ForeignKey(Central_department, on_delete = models.CASCADE)
+	authority_name = models.CharField(max_length = 200)
+
+class Central_RTI_query(models.Model):
+	def __unicode__(self):
+		return self.rti_query
+
+	rti_query = models.ForeignKey(RTI_query, primary_key = True)
+	department = models.ForeignKey(Central_department, on_delete = models.SET_DEFAULT, default = 1)
+	authority = models.ForeignKey(Central_authority, on_delete = models.SET_NULL, null = True)
+
+# RESPONSE
+class RTI_response(models.Model):
+	def __unicode__(self):
+		return self.description
+
+	rti_query = models.ForeignKey(RTI_query, primary_key = True, on_delete = models.CASCADE)
+	response_text = models.TextField()
+	description = models.CharField(max_length = 200)
+	# check for response date > query date
+	rti_response_date = models.DateTimeField(null = True)
+	entry_date = models.DateTimeField()
+	update_date = models.DateTimeField(auto_now_add=True)
+	
+class Tag(models.Model):
+	def __unicode__(self):
+		return self.tag_text
+	tag_text = models.CharField(max_length = 200)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class RTI_tag(models.Model):
+	def __unicode__(self):
+		return self.tag
+
+	rti_query = models.ForeignKey(RTI_query, on_delete = models.CASCADE)
+	tag = models.ForeignKey(Tag, on_delete = models.CASCADE)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class User_tag(models.Model):
+	def __unicode__(self):
+		return self.tag
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	tag = models.ForeignKey(Tag, on_delete = models.CASCADE)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+	def __unicode__(self):
+		return self.rti_query
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	rti_query = models.ForeignKey(RTI_query, on_delete = models.CASCADE)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class Share(models.Model):
+	def __unicode__(self):
+		return self.rti_query
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	rti_query = models.ForeignKey(RTI_query, on_delete = models.CASCADE)
+	share_text = models.TextField(null = True)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	rti_query = models.ForeignKey(RTI_query, on_delete = models.CASCADE)
+	commment_text = models.TextField(null = True)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+
+class Follow_query(models.Model):
+	def __unicode__(self):
+		return self.rti_query
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	rti_query = models.ForeignKey(RTI_query, on_delete = models.CASCADE)
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class Follow_user(models.Model):
+	def __unicode__(self):
+		return self.follower
+
+	follower = models.ForeignKey(User, on_delete = models.CASCADE, related_name='follower')
+	followee = models.ForeignKey(User, on_delete = models.CASCADE, related_name='followee')
+	entry_date = models.DateTimeField(auto_now_add=True)
+
+class Notification(models.Model):
+	def __unicode__(self):
+		return self.follower
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='notified')
+	notification_text = models.CharField(max_length = 200)
+	notification_url = models.CharField(max_length = 300)
+	notification_type = models.CharField(max_length = 200)
+	notification_status = models.BooleanField(default = False)
+	seen_date = models.DateTimeField(null = True)
+	rti_query = models.ForeignKey(RTI_query, null = True, on_delete = models.SET_NULL)
+	other_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='notified_by')
+
+class Relevance(models.Model):
+	def __unicode__(self):
+		return self.relevance
+
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	rti_query = models.ForeignKey(RTI_query, on_delete = models.CASCADE)
+	relevance = models.FloatField(default = 0.0)
+	views = models.IntegerField(default = 0)
+	update_date = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+
+
+	
+
+
+	
