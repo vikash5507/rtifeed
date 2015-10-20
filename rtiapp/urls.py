fdbc0d3d4f4c7da6ec961d24ada6392a9765873a
Paarth <paarth.n@gmail.com>
2015-10-21 00:14:29 +0530

    structured code

--- rtiapp/urls.py
+++ rtiapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
-from rtiapp import views
+from rtiapp.views import views_test
 urlpatterns = patterns('',
-	url(r'^fblogintest', views.fblogintest, name = 'fblogintest'),
-	url(r'^logintest', views.logintest, name = 'logintest'),
+	url(r'^fblogintest', views_test.fblogintest, name = 'fblogintest'),
+	url(r'^logintest', views_test.logintest, name = 'logintest'),
 )
